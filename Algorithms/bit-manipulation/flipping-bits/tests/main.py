2b347cab304e9f021c16834b7ce60a42fbbc6d00
diegotpereira <diegoteixeirapereira@hotmail.com>
2023-11-17 11:50:24 -0300

    criação das soluções python

--- Algorithms/bit-manipulation/flipping-bits/tests/main.py
+++ Algorithms/bit-manipulation/flipping-bits/tests/main.py
@@ -1,13 +1,26 @@
+
+# Importa a função flippingBits do módulo flippingBits
+
 from flippingBits import flippingBits
 
+# Verifica se o script está sendo executado como um programa principal
 if __name__ == '__main__':
     
+    # Solicita ao usuário que insira um valor inteiro e remove espaços 
+    # em branco desnecessários
     entrada = int(input().strip())
     
+    # Loop que itera através do número de casos de teste 
+    # especificados por 'entrada'
     for qItr in range(entrada):
         
+        # Solicita ao usuário que insira um número inteiro 
+        # e remove espaços em branco desnecessários
         n = int(input().strip())
         
+        # Chama a função flippingBits com o valor 
+        # de 'n' e armazena o resultado
         resultado = flippingBits(n)
         
+        # Imprime o resultado seguido por uma nova linha
         print(str(resultado) + '\n')
